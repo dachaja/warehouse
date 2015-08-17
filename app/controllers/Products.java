e228d903322cceb025320680efb3d6c8c05a2af9
dachaja <pettitbusiness@gmail.com>
2015-08-17 00:35:47 -0700

    added product list feature.

---
+++ app/controllers/Products.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import java.util.Set;
+
+import models.Product;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
+public class Products extends Controller{
+	public Result list() {
+		Set<Product> products = Product.findAll();
+		return ok(list.render(products));
+	}
+	public Result showBlank() {
+		return TODO;
+	}
+	public Result show(Long ean) {
+		return TODO;
+	}
+	public Result save() {
+		return TODO;
+	}
+}
