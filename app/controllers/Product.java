545775af146e32d2193d90c99a9fbf25c6d2f460
dachaja <pettitbusiness@gmail.com>
2015-10-11 14:41:23 -0700

    added methods in product controller

--- app/controllers/Product.java
+++ app/controllers/Product.java
@@ -1,21 +1,35 @@
 package controllers;
 
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 import models.StockItem;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
 public class Product extends Controller{
-	public Result list(Long warehouseId) {
-		List<StockItem> stockItems = StockItem.find()
-				.where()
-				.eq("warehouse_id", warehouseId)
-				.findList();
-		if(request().accepts("text/plain")) {
-			return ok(StringUtils.join(stockItems, "\n"));
-		}
-		return ok(product.render(stockItems));
+	public Result index() {
+		return redirect(routes.Product.list(1));
 	}
+	public Result list(int page) {
+		return ok();
+	}
+	public Result details(long ean) {
+		return TODO;
+	}
+	public Result edit(long ean) {
+		return TODO;
+	}
+	public Result save(long ean) {
+		return TODO;
+	}
+//	public Result list(Long warehouseId) {
+//		List<StockItem> stockItems = StockItem.find()
+//				.where()
+//				.eq("warehouse_id", warehouseId)
+//				.findList();
+//		if(request().accepts("text/plain")) {
+//			return ok(StringUtils.join(stockItems, "\n"));
+//		}
+//		return ok(product.render(stockItems));
+//	}
 }
